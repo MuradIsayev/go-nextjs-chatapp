072a8f0c1eea3b3e01097c2e3be84ae2f141fa87
MuradIsayev <muradisayevazer@gmail.com>
2024-01-30 16:07:54 +0400

    [server] feat: initialize user module and router

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,11 +4,21 @@ import (
 	"log"
 
 	"github.com/MuradIsayev/go-nextjs-chatapp/db"
+	"github.com/MuradIsayev/go-nextjs-chatapp/internal/user"
+	"github.com/MuradIsayev/go-nextjs-chatapp/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Could't initialize database connection: %s", err.Error())
 	}
+
+	userRepository := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHandler(userService)
+
+	router.InitRouter(userHandler)
+	router.Start("localhost:8080")
+
 }
